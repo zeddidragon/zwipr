15577a8bd859479e2348a567a5f0401d812676ae
Tony A. Sæle <tony.saele@scoutggaminggroup.com>
2022-05-20 06:55:39 +0700

    also enable pointer-events

--- zwipr.js
+++ zwipr.js
@@ -1,4 +1,7 @@
-console.log("zwipr loaded")
 document
   .getElementsByClassName("spaceball")[0]
   .setAttribute("style", "display: none");
+document.getElementById("allsizes-photo")
+  .getElementsByTagName("img")[0]
+  .setAttribute("style", "pointer-events: all")
+console.log("zwipr loaded")
